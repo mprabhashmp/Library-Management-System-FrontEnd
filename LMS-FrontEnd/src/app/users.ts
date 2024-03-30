a189c5fdcd609471a1352783ea69a62e9fcd9976
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2024-03-30 15:21:51 +0530

    users

---
+++ LMS-FrontEnd/src/app/users.ts
@@ -0,0 +1,11 @@
+export interface User {
+    username: string;
+    name:string;
+    email:string;
+    contact_number:string;
+    dob:string;
+    nic:string;
+    address:string;
+    gender:string;
+    
+  }

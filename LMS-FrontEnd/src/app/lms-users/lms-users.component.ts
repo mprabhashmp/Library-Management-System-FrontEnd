0ee7c601accfbeddd5186b068730bfbf75e3ebcb
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2024-04-12 10:33:52 +0530

    xr

--- LMS-FrontEnd/src/app/lms-users/lms-users.component.ts
+++ LMS-FrontEnd/src/app/lms-users/lms-users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LmsUsersService } from './lms-users.service';
 
 @Component({
   selector: 'app-lms-users',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class LmsUsersComponent {
 
+
+  users: any[] = [];
+  userId: any;
+
+  constructor(private userService: LmsUsersService) { }
+
+  ngOnInit(): void {
+    this.fetchAllUsers();
+  }
+
+  fetchAllUsers(): void {
+    this.userService.getAllUsers().subscribe(
+      (data: any) => {
+        this.users = data;
+      },
+      (error: any) => {
+        console.error('Error fetching users:', error);
+        // Handle error as needed
+      }
+    );
+  }
+
+  // deleteUser(userId: any): void {
+  //   if (confirm('Are you sure you want to delete this user?')) {
+  //     this.userService.deleteUser(userId).subscribe(
+  //       () => {
+  //         // Filter out the deleted user from the list
+  //         this.users = this.users.filter(user => user.id !== userId);
+  //         alert('User deleted successfully');
+  //       },
+  //       (error: any) => {
+  //         console.error('Error deleting user:', error);
+  //         // Handle error as needed
+  //       }
+  //     );
+  //   }
+  //}
 }

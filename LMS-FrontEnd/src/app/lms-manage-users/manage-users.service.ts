9a8334db17af10abcb32b20240e72cb8654c8cbe
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2024-04-12 10:33:46 +0530

    updated435

---
+++ LMS-FrontEnd/src/app/lms-manage-users/manage-users.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ManageUsersService {
+
+  private apiUrl = 'http://localhost:8080/api/getusers'; // Replace with your actual API URL
+
+  constructor(private http: HttpClient) { }
+
+  // Get all users
+  getAllUsers(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  // Delete user by ID
+  deleteUser(userId: number): Observable<any> {
+    const deleteUrl = `${this.apiUrl}/${userId}`;
+    return this.http.delete(deleteUrl);
+  }
+}
